537bce5bbc783bedb98b824a047e5b24196d6eaf
Asnaebeya <asnaebeya@gmail.com>
2019-10-20 13:12:20 +0700

    gridpush

--- public/game.js
+++ public/game.js
@@ -7,13 +7,41 @@ var temp = parameters[0].split("=");
 var name = unescape(temp[1]);
 nameSpan.textContent = name;
 
-//Create socket 
+//Create sockets
 var socket = io.connect('/game');
 
-//Add new player
+/*----------Documentation----------*/
+
+//--These are what you can sent to the server--//
+
+//1. Add new player
 socket.emit('add_player',name);
 
-//Listen for events from server
+//2. Increase point
+var button = document.getElementById('increase_point');
+button.addEventListener('click',()=>{
+    socket.emit('increase_point',1);
+});
+
+//3. Get words
+socket.emit('get_words',{
+    'duration': 10, //sec
+    'rate': 2 //words per sec
+});
+
+//--These are what you can listen for--//
+
+//1. Changes in number of players
 socket.on('number_of_players_changed',(value)=>{
     console.log(value);
+});
+
+//2. Set of words you requested
+socket.on('words',(value)=>{
+    console.log(value);
+});
+
+//3. Force reset
+socket.on('reset_game',()=>{
+    console.log('reset the game');
 });
