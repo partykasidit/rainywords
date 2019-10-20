1ff9c72ccb01a33b6e8ee876de211cc6a8df7d52
Asnaebeya <asnaebeya@gmail.com>
2019-10-20 13:29:47 +0700

    Gridadd2

--- client/src/App.js
+++ client/src/App.js
@@ -1,50 +1,86 @@
-import React, { useState, useEffect, Component } from 'react';
-import logo from './logo.svg';
-import WordBox, {MovingWordBox, CoolBox} from './WordBox';
-import './App.css';
-import Timer from './Timer';
+import React, { useState, useEffect, Component } from "react";
+import logo from "./logo.svg";
+import WordBox, { MovingWordBox, CoolBox } from "./WordBox";
+import "./App.css";
+import Timer from "./Timer";
 
 function App() {
   const [count, setCount] = useState(0);
   const [words, setWords] = useState([]);
-  
-  const [input,setInput] = useState('')
+
+  const [input, setInput] = useState("");
   const handleInput = e => {
-    setInput(e.target.value)
-  }
+    setInput(e.target.value);
+  };
   const handleSubmit = e => {
     //setWords(old => old.filter(w => w !== input))
     setWords(oldWords => {
-      const newWords = []
-      for(const word of oldWords) {
-        if(word !== input) {
+      const newWords = [];
+      for (const word of oldWords) {
+        if (word.word !== input) {
           // generate x value with word remove word from old array
-          newWords.push(word)
-        }else{
-          setCount(c => c+1)
+          newWords.push(word);
+        } else {
+          setCount(c => c + 1);
         }
       }
-      return newWords
-    })
-    setInput('')
-    e.preventDefault()
-  }
-  const randomWords = ['asd','asdfg','hheksdf','kkkkk','Ronaldo']
+      return newWords;
+    });
+    setInput("");
+    e.preventDefault();
+  };
+  const randomWords = ["Gareth", "Ronaldo", "Falcao", "kkkkk", "Ronaldo",'BigC'];
+  let inputRef;
   useEffect(() => {
-    let index = 0
-    const size = randomWords.length
+    let index = 0;
+    const size = randomWords.length;
     setInterval(() => {
-      setWords(old => [...old, randomWords[(index++%size)]])
+      setWords(old => [
+        ...old,
+        {
+          id: index,
+          word: randomWords[index++ % size],
+          location: Math.floor(Math.random() * 60) + 20 + "vw"
+        }
+      ]);
       console.log(words);
-    },5000);
-  }, [])
+    }, 5000);
+
+    inputRef.focus();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-      <Timer startCount ='30'/>
-      <p>{count}</p>
-        {/* <p>
+        <Timer startCount="30" />
+        <p>{count}</p>
+        <form onSubmit={handleSubmit}>
+          <input
+            onChange={handleInput}
+            value={input}
+            ref={input => {
+              inputRef = input;
+            }}
+          />
+        </form>
+      
+        {words.map(({ word, location, id }, idx) => (
+          <CoolBox
+            className="thebox"
+            word={word}
+            location={location}
+            key={idx}
+            onDropped={() => setWords(old => old.filter(w => w.id !== id))}
+          ></CoolBox>
+        ))}
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+{/* <p>
           You learn React {count} time
         </p>
         <a
@@ -54,18 +90,3 @@ function App() {
         >
           Learn React
         </a> */}
-        {
-          words.map((word, idx) =>  
-            <CoolBox className="thebox" word={word} key={idx}></CoolBox> 
-          )
-        }
-        
-      </header> 
-      <form  onSubmit={handleSubmit}>
-     <input onChange={handleInput} value={input}/>
-      </form>
-    </div>
-  );
-}
-
-export default App;
