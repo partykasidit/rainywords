02f24d75560aa5eac38fc60b7c76553a121864fe
Asnaebeya <asnaebeya@gmail.com>
2019-10-25 01:49:05 +0700

    FinishGameV2

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,7 +1,9 @@
 import openSocket from "socket.io-client";
 
-const url = "localhost:4000/game";
+const url = "http://localhost:4000/game";
 //https://rainy-words-server.herokuapp.com/admin/
 // https://rainy-words-server.herokuapp.com/game
 //https://rainy-words-server.herokuapp.com/admin/
+// const url = "http://localhost:4000/game";	const url = "https://rainy-words-server.herokuapp.com/game";
+// //172.20.10.4:4000/game
 export const socket = openSocket(url);
