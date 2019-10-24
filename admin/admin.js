5801f707af4e968dde85c3cb0b72bd81f5126ac7
partykasidit <kasidit.09@hotmail.com>
2019-10-24 17:35:22 +0700

    Manual "prettier" to avoid merge conflict

--- admin/admin.js
+++ admin/admin.js
@@ -1,47 +1,48 @@
 //Create socket
-var socket = io.connect('http://localhost:4000/game');
+var socket = io.connect("http://localhost:4000/game");
 
 //DOM
-var resetButton = document.getElementById('reset_button');
+var resetButton = document.getElementById("reset_button");
 var numberOfPlayersSpan = document.getElementsByTagName("span")[0];
-var scoresDiv = document.getElementById('scores');
+var scoresDiv = document.getElementById("scores");
 
-resetButton.addEventListener("click",()=>{
-    socket.emit('reset_game');
+resetButton.addEventListener("click", () => {
+    socket.emit("reset_game");
 });
 
 //Get number of players
-socket.emit('get_number_of_players');
+socket.emit("get_number_of_players");
 
 //Listen for changes in number of players
-socket.on('number_of_players_changed',(value)=>{
+socket.on("number_of_players_changed", value => {
     numberOfPlayersSpan.textContent = value;
-})
+});
 
 //Get players
-socket.emit('get_players');
+socket.emit("get_players");
 
 //Listen for changes in scores/players
-socket.on('players_changed',(players)=>{
+socket.on("players_changed", players => {
     updateTable(players);
 });
 
 function updateTable(players) {
     const keys = Object.keys(players);
-    if(keys.length > 0) {
+    if (keys.length > 0) {
         var string = '<table style="width=100%">';
-        string += "<tr><th>Player</th><th>Score</th><tr>"
-        for(const key of keys) {
+        string += "<tr><th>Player</th><th>Score</th><tr>";
+        for (const key of keys) {
             var player = players[key];
-            string += "<tr><td>" + player.username + "</td><td>" + player.score + "</td></tr>";
+            string +=
+                "<tr><td>" +
+                player.username +
+                "</td><td>" +
+                player.score +
+                "</td></tr>";
         }
-        string += "</table>"
+        string += "</table>";
         scoresDiv.innerHTML = string;
-    }else{
+    } else {
         scoresDiv.innerHTML = "";
     }
 }
-
-
-
-
