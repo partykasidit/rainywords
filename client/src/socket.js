74db2d4a80181741bc2dd110e98740a46660d367
partykasidit <kasidit.09@hotmail.com>
2019-10-25 10:42:28 +0700

    change to localhost

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import openSocket from "socket.io-client";
 
 const url = "http://localhost:4000/game";
+// const url = "https://rainy-words-server.herokuapp.com/game";
 //https://rainy-words-server.herokuapp.com/admin/
 // https://rainy-words-server.herokuapp.com/game
 //https://rainy-words-server.herokuapp.com/admin/
